8a7df4fe8753123d3c5b09010e6f41b6afb2f5ec
uliana <brainydamage@gmail.com>
2019-11-26 18:40:02 +0300

    Maven: Step1

---
+++ src/main/java/com/epam/javacourse/maven/Step1.java
@@ -0,0 +1,7 @@
+package com.epam.javacourse.maven;
+
+public class Step1 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ src/test/java/com/epam/javacourse/maven/Step1Test.java
@@ -0,0 +1,9 @@
+package com.epam.javacourse.maven;
+
+import org.testng.annotations.Test;
+
+public class Step1Test {
+    @Test
+    public void testMain() {
+    }
+}
